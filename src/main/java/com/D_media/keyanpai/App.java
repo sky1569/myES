ec820088020b9a430788bbf806899a172e840b42
sky1569 <niyaotian@163.com>
2015-05-23 14:54:07 +0800

    helloworld!!!!!!!11

--- src/main/java/com/D_media/keyanpai/App.java
+++ src/main/java/com/D_media/keyanpai/App.java
@@ -29,7 +29,7 @@ public class App
 	
     public static void main( String[] args ) 
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!!!!!!!!!!!!!!!!!" );
         
        	List<String> clusterList = new ArrayList<String>();
     	//一个集群
